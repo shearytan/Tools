6f8282c0d0ac2f3b23dfa5361b2c358f8d09a48e
Sheary Tan <sheary_99@yahoo.com>
2018-09-13 12:23:13 +0800

    SVG edited

--- src/Components/Apple/Apple.js
+++ src/Components/Apple/Apple.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react'
 
+import Popover from '@material-ui/core/Popover';
+
 import AppleLogo from './apple.svg';
+import './Apple.css';
 
 export default class Apple extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+    }
+  }
+
+  handleClick = e => {
+    this.setState({anchorEl: e.currentTarget})
+  }
+
+  handleClose = e => {
+    this.setState({anchorEl: null});
+  }
+
   render() {
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
     return (
       <div>
-        <img src={AppleLogo} />
+        <Popover
+          open={open}
+          anchorEl={anchorEl}
+          onClose={this.handleClose}
+          anchorOrigin={{vertical: 'center',}}
+          transformOrigin={{
+            vertical: 'top',
+          }}
+        >
+          <span className="popover">Just a simple SVG!</span>
+        </Popover>
+        <div className="apple-container" onClick={this.handleClick}>
+          <img src={AppleLogo} className="apple"/>
+        </div>
       </div>
     )
   }
