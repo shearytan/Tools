7180fdd63b6d49061830ffdf6ba29aa3ed44837e
Sheary Tan <sheary_99@yahoo.com>
2018-09-19 19:57:13 +0800

    board and box created

--- src/Components/Tic-Tac-Toe/index.js
+++ src/Components/Tic-Tac-Toe/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
+import './styles/index.css';
+
 export default class TicTacToe extends Component {
   render() {
     return (
       <div>
-        This is ticky tacky toe
+        <div className="board">
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+          
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+
+          <div className="box"></div>
+          <div className="box"></div>
+          <div className="box"></div>
+        </div>
       </div>
     )
   }
 }
+
+// Create 9 boxes
