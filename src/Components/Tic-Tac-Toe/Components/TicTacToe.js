8dd20cafacb4db085fd4eeaca6e5032747ca6db4
Sheary Tan <sheary_99@yahoo.com>
2018-09-22 09:39:14 +0800

    tic tac toe winning

--- src/Components/Tic-Tac-Toe/Components/TicTacToe.js
+++ src/Components/Tic-Tac-Toe/Components/TicTacToe.js
@@ -22,31 +22,36 @@ export default class Board extends Component {
     }
 
     let winner = this.handleWinner();
-    console.log("This is gameState: ", this.gameState)
+    console.log("Winner is: ", winner)
   }
 
   handleWinner = (e) => {
       const winPath = [[0, 1, 2], [0, 3, 6], [6, 7, 8], [2, 5, 8], [0, 4, 8], [2, 4, 6], [1, 4, 7], [3, 4, 5]];
       let board = this.gameState.board;
       for(let i = 0; i < winPath.length; i++) {
-          console.log("This is board: ", board[winPath[i][0]])
+        if(board[winPath[i][0]] === board[winPath[i][1]] && board[winPath[i][1]] === board[winPath[i][2]]) {
+          return board[winPath[i][0]];
+        }
+        if(this.gameState.totalMove === 9) {
+          return "draw"
+        }
       }
   }
 
   render() {
     return (
         <div className="board" onClick={(e) => this.handleClick(e.target)}>
+          <div className="box" data-square="0"></div>
           <div className="box" data-square="1"></div>
           <div className="box" data-square="2"></div>
-          <div className="box" data-square="3"></div>
           
+          <div className="box" data-square="3"></div>
           <div className="box" data-square="4"></div>
           <div className="box" data-square="5"></div>
-          <div className="box" data-square="6"></div>
 
+          <div className="box" data-square="6"></div>
           <div className="box" data-square="7"></div>
           <div className="box" data-square="8"></div>
-          <div className="box" data-square="9"></div>
         </div>
     )
   }
